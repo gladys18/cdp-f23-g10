6b5adcb947410e01a5a8446c5ce62e33c70601ae
nguemo12 <aymardnguemo1234@gmail.com>
2024-04-03 13:06:33 +0200

    fsd

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import "../../styles/about-section.css";
 import aboutImg from "../../assets/all-images/boats-img/image6.jpeg";
+import persona from "../../assets/all-images/persona.png"
 
 const AboutSection = ({ aboutClass }) => {
   return (
@@ -17,10 +18,17 @@ const AboutSection = ({ aboutClass }) => {
         <Row>
           <Col lg="6" md="6">
             <div className="about__section-content">
-              <h4 className="section__subtitle">À propos de nous</h4>
-              <h2 className="section__title">Bienvenue chez SAILINGLOC</h2>
+              <div className="d-flex align-items-center gap-2">
+                <div>
+                  <h4 className="section__subtitle">À propos de nous</h4>
+
+                  <h2 className="section__title">Bienvenue chez SAILINGLOC</h2>
+                </div>
+                <img src={persona} style={{width:"40px"}} />
+              </div>
+
               <p className="section__description">
-              Chez Sailingloc , nous sommes passionnés par la voile et déterminés à partager cette passion avec nos clients. Forts d’expérience dans l’industrie maritime, nous avons créé notre entreprise pour offrir des expériences de navigation inoubliables à tous ceux qui cherchent à explorer les merveilles de l’océan. En tant que propriétaire, je met un point d’honneur à garantir que chaque voyage en mer avec nous soit non seulement sûr et confortable, mais aussi empreint de moments magiques et de découvertes inattendues. Nous sommes impatients de vous accueillir à bord et de vous aider à créer des souvenirs précieux sur les flots.
+                Chez Sailingloc , nous sommes passionnés par la voile et déterminés à partager cette passion avec nos clients. Forts d’expérience dans l’industrie maritime, nous avons créé notre entreprise pour offrir des expériences de navigation inoubliables à tous ceux qui cherchent à explorer les merveilles de l’océan. En tant que propriétaire, je met un point d’honneur à garantir que chaque voyage en mer avec nous soit non seulement sûr et confortable, mais aussi empreint de moments magiques et de découvertes inattendues. Nous sommes impatients de vous accueillir à bord et de vous aider à créer des souvenirs précieux sur les flots.
               </p>
 
               <div className="about__section-item d-flex align-items-center">
@@ -29,7 +37,7 @@ const AboutSection = ({ aboutClass }) => {
                 </p>
 
                 <p className="section__description d-flex align-items-center gap-2">
-                  <i class="ri-checkbox-circle-line"></i> Trouvez votre voilier parfait 
+                  <i class="ri-checkbox-circle-line"></i> Trouvez votre voilier parfait
                 </p>
               </div>
 
