f7dce70a5e7afabe26950455d213cff897a0cc10
nguemo12 <aymardnguemo1234@gmail.com>
2024-04-17 18:51:30 +0200

    dds

--- src/components/UI/CarItem.jsx
+++ src/components/UI/CarItem.jsx
@@ -9,12 +9,13 @@ const CarItem = (props) => {
   return (
     <Col lg="4" md="4" sm="6" className="mb-5">
       <div className="car__item">
+      <h4 className="section__title text-center">{carName}</h4>
         <div className="car__img">
           <img src={imgUrl} alt="" className="w-100" />
         </div>
 
         <div className="car__item-content mt-4">
-          <h4 className="section__title text-center">{carName}</h4>
+         
           <h6 className="rent__price text-center mt-">
             {price}.00€ <span>/ Jour</span>
           </h6>
